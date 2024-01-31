70d5bb4964ccb52a4a9e3d4aa6d1fdd91ec33573
Artsiom Vasilyeu <vslvrtm@gmail.com>
2024-01-31 19:51:44 +0400

    Commit 1

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -6,7 +6,7 @@ package app;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello workd form test!!!");
+        System.out.println("Hello workd 1!");
     }
     
 }
